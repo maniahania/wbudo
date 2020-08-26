022442fa8b3586124ac46d01be3f8609da07a7b9
maniahania <a.strzelecka@onet.pl>
2020-08-26 20:18:23 +0200

    guzik

---
+++ app/src/main/java/com/example/riddles/Riddle1.kt
@@ -0,0 +1,11 @@
+package com.example.riddles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Riddle1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_riddle1)
+    }
+}
