01fdda54fdfb8be825aafbd8d6b62d136c95502f
maniahania <a.strzelecka@onet.pl>
2020-08-27 12:57:54 +0200

    ostatni guzik

--- app/src/main/java/com/example/riddles/Riddle6.kt
+++ app/src/main/java/com/example/riddles/Riddle6.kt
@@ -1,11 +1,19 @@
 package com.example.riddles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Riddle6 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_riddle6)
+
+        val button7 = findViewById<Button>(R.id.button7)
+        button7.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
