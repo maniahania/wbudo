5e7b241ba81871c1db457a927ab5144dae457068
maniahania <a.strzelecka@onet.pl>
2020-08-27 16:44:23 +0200

    light sensor zrobiony mniej wiecej

--- app/src/main/java/com/example/riddles/Riddle2.kt
+++ app/src/main/java/com/example/riddles/Riddle2.kt
@@ -1,19 +1,58 @@
 package com.example.riddles
 
-import android.content.Intent
+import android.content.Context
+import  android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_riddle2.*
 
-class Riddle2 : AppCompatActivity() {
+class Riddle2 : AppCompatActivity(),SensorEventListener {
+    var sensor: Sensor? = null
+    var sensorManager: SensorManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_riddle2)
 
         val button3 = findViewById<Button>(R.id.button3)
+        button3.visibility = View.INVISIBLE
         button3.setOnClickListener{
             val intent = Intent(this, Riddle3::class.java)
             startActivity(intent)
         }
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun onSensorChanged(event: SensorEvent?) {
+            if(event!!.values[0] <30 )
+            {
+                button3.visibility = View.VISIBLE
+                textView.visibility = View.INVISIBLE
+                textView2.visibility = View.INVISIBLE
+            } else {
+                button3.visibility = View.INVISIBLE
+                textView.visibility = View.VISIBLE
+                textView2.visibility = View.VISIBLE
+            }
+    }
+
+    override fun onResume() {
+        sensorManager!!.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL)
+        super.onResume()
+    }
+
+    override fun onPause() {
+        sensorManager!!.unregisterListener(this)
+        super.onPause()
     }
 }
